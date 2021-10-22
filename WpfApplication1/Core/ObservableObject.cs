b65d9243c7a5a8f782d24862a9add068fa1b60fd
Drenhaus <marc.frederic.drenhaus@estudiantat.upc.edu>
2021-10-22 13:50:03 +0200

    fondo i redendo

---
+++ WpfApplication1/Core/ObservableObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WpfApplication1.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName])
+    }
+}
